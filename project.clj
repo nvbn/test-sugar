e15957ca56050ce912cba8e4aaea3cd8330e3fef
nvbn <nvbn.rm@gmail.com>
2014-08-16 19:31:59 +0900

    Fix typo in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject test-sugar "1.0"
   :description "Sugar for clojure.test"
-  :url "https://github.com/nvbn/test-sugare/"
+  :url "https://github.com/nvbn/test-sugar/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
